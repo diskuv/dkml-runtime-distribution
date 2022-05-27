4776416b564967a2fe287502dc9e02e43dd67581
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2022-05-26 19:32:25 -0700

    Bump version: 0.4.0-prerel57 → 0.4.0-prerel58

--- src/unix/create-opam-switch.sh
+++ src/unix/create-opam-switch.sh
@@ -38,8 +38,8 @@ PINNED_PACKAGES_DKML_PATCHES="
     ppx_expect,v0.14.1
 
     dkml-c-probe,2.0.0~prerel3
-    dkml-apps,0.4.0~prerel57
-    opam-dkml,0.4.0~prerel57
+    dkml-apps,0.4.0~prerel58
+    opam-dkml,0.4.0~prerel58
     "
 
 # These MUST BE IN SYNC with vendor/drd/src/unix/private/ml/ocaml_opam_repo_trim.ml's [packages_fdopen_to_remove].
