eded51b5cd040ca08c3f221e078279fa628bc08a
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2022-06-11 21:16:19 -0700

    Bump version: 0.4.0-prerel70 → 0.4.0-prerel71

--- src/unix/create-opam-switch.sh
+++ src/unix/create-opam-switch.sh
@@ -37,8 +37,8 @@ PINNED_PACKAGES_DKML_PATCHES="
     ocamlbuild,0.14.0
     ppx_expect,v0.14.1
 
-    dkml-apps,0.4.0~prerel70
-    opam-dkml,0.4.0~prerel70
+    dkml-apps,0.4.0~prerel71
+    opam-dkml,0.4.0~prerel71
     "
 
 # These MUST BE IN SYNC with vendor/drd/src/unix/private/ml/ocaml_opam_repo_trim.ml's [packages_fdopen_to_remove].
