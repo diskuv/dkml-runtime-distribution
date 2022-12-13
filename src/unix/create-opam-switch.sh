1c28db0a5d60095cf4fab325a396fe06cf911c47
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2022-12-12 20:32:32 -0800

    Bump version: 1.0.2-prerel28 → 1.0.2-prerel29

--- src/unix/create-opam-switch.sh
+++ src/unix/create-opam-switch.sh
@@ -32,9 +32,9 @@ PINNED_PACKAGES_DKML_PATCHES="
     ppx_expect,v0.14.1
     yaml,3.0.1+windows+r1
 
-    dkml-apps,1.0.2~prerel28
-    opam-dkml,1.0.2~prerel28
-    with-dkml,1.0.2~prerel28
+    dkml-apps,1.0.2~prerel29
+    opam-dkml,1.0.2~prerel29
+    with-dkml,1.0.2~prerel29
     "
 
 # These MUST BE IN SYNC with vendor/drd/src/ml/ocaml_opam_repo_trim.ml's [packages_fdopen_to_remove].
