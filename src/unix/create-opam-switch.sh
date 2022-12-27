7affdc187caee5e17bca88cd039bc8dd2f9a4924
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2022-12-27 11:32:05 -0800

    Finish 1.1.0 release (2 of 2)

--- src/unix/create-opam-switch.sh
+++ src/unix/create-opam-switch.sh
@@ -31,9 +31,9 @@ PINNED_PACKAGES_DKML_PATCHES="
     ppx_expect,v0.15.1
     yaml,3.0.1+windows+r1
 
-    dkml-apps,1.1.0~prerel27
-    dkml-exe,1.1.0~prerel27
-    with-dkml,1.1.0~prerel27
+    dkml-apps,1.1.0
+    dkml-exe,1.1.0
+    with-dkml,1.1.0
     "
 
 # These MUST BE IN SYNC with vendor/drd/src/ml/ocaml_opam_repo_trim.ml's [packages_fdopen_to_remove].
