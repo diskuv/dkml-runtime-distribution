4f1146c0dba6b48b2e29b6c8e559dd19937764ed
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2023-06-30 04:15:12 -0700

    Rename to pkg/bump

--- bump-packages.cmake
+++ bump-packages.cmake
@@ -2,7 +2,7 @@
 # the 'opam' executable must be in the PATH.
 #
 # Test with:
-# cmake --log-context -D DRYRUN=1 -D CMAKE_MESSAGE_CONTEXT=dkml-runtime-distribution -D DKML_RELEASE_IS_UPGRADING_PACKAGES=1 -D GIT_EXECUTABLE=git -D DKML_RELEASE_DUNE_VERSION=3.6.2 -D DKML_RELEASE_PARTICIPANT_MODULE=../../../packaging/version-bump/DkMLReleaseParticipant.cmake -P bump-packages.cmake
+# cmake --log-context -D DRYRUN=1 -D CMAKE_MESSAGE_CONTEXT=dkml-runtime-distribution -D DKML_RELEASE_IS_UPGRADING_PACKAGES=1 -D GIT_EXECUTABLE=git -D DKML_RELEASE_DUNE_VERSION=3.6.2 -D DKML_RELEASE_PARTICIPANT_MODULE=../../../pkg/bump/DkMLReleaseParticipant.cmake -P bump-packages.cmake
 
 if(NOT DKML_RELEASE_PARTICIPANT_MODULE)
     message(FATAL_ERROR "Missing -D DKML_RELEASE_PARTICIPANT_MODULE=.../DkMLReleaseParticipant.cmake")
--- bump-version.cmake
+++ bump-version.cmake
@@ -1,5 +1,5 @@
 # Test with:
-# cmake --log-context -D DRYRUN=1 -D CMAKE_MESSAGE_CONTEXT=dkml-runtime-distribution -D "regex_DKML_VERSION_SEMVER=1[.]2[.]1-[0-9]+" -D "regex_DKML_VERSION_OPAMVER=1[.]2[.]1[~]prerel[0-9]+" -D DKML_VERSION_SEMVER_NEW=1.2.1-3 -D DKML_VERSION_OPAMVER_NEW=1.2.1~prerel3 -D GIT_EXECUTABLE=git -D DKML_RELEASE_OCAML_VERSION=4.14.0 -D DKML_RELEASE_PARTICIPANT_MODULE=../../../packaging/version-bump/DkMLReleaseParticipant.cmake -P bump-version.cmake
+# cmake --log-context -D DRYRUN=1 -D CMAKE_MESSAGE_CONTEXT=dkml-runtime-distribution -D "regex_DKML_VERSION_SEMVER=1[.]2[.]1-[0-9]+" -D "regex_DKML_VERSION_OPAMVER=1[.]2[.]1[~]prerel[0-9]+" -D DKML_VERSION_SEMVER_NEW=1.2.1-3 -D DKML_VERSION_OPAMVER_NEW=1.2.1~prerel3 -D GIT_EXECUTABLE=git -D DKML_RELEASE_OCAML_VERSION=4.14.0 -D DKML_RELEASE_PARTICIPANT_MODULE=../../../pkg/bump/DkMLReleaseParticipant.cmake -P bump-version.cmake
 
 if(NOT DKML_RELEASE_PARTICIPANT_MODULE)
     message(FATAL_ERROR "Missing -D DKML_RELEASE_PARTICIPANT_MODULE=.../DkMLReleaseParticipant.cmake")
