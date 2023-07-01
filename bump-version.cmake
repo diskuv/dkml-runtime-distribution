9bfad27e9389ebbc23bf0421259cc18f98a7c099
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2023-06-30 22:30:02 -0700

    Rename DkMLReleaseParticipant to DkMLBumpVersionParticipant

--- bump-version.cmake
+++ bump-version.cmake
@@ -1,14 +1,14 @@
 # Test with:
-# cmake --log-context -D DRYRUN=1 -D CMAKE_MESSAGE_CONTEXT=dkml-runtime-distribution -D "regex_DKML_VERSION_SEMVER=1[.]2[.]1-[0-9]+" -D "regex_DKML_VERSION_OPAMVER=1[.]2[.]1[~]prerel[0-9]+" -D DKML_VERSION_SEMVER_NEW=1.2.1-3 -D DKML_VERSION_OPAMVER_NEW=1.2.1~prerel3 -D GIT_EXECUTABLE=git -D DKML_RELEASE_OCAML_VERSION=4.14.0 -D DKML_RELEASE_PARTICIPANT_MODULE=../../../pkg/bump/DkMLReleaseParticipant.cmake -P bump-version.cmake
+# cmake --log-context -D DRYRUN=1 -D CMAKE_MESSAGE_CONTEXT=dkml-runtime-distribution -D "regex_DKML_VERSION_SEMVER=1[.]2[.]1-[0-9]+" -D "regex_DKML_VERSION_OPAMVER=1[.]2[.]1[~]prerel[0-9]+" -D DKML_VERSION_SEMVER_NEW=1.2.1-3 -D DKML_VERSION_OPAMVER_NEW=1.2.1~prerel3 -D GIT_EXECUTABLE=git -D DKML_RELEASE_OCAML_VERSION=4.14.0 -D DKML_RELEASE_PARTICIPANT_MODULE=../../../pkg/bump/DkMLBumpVersionParticipant.cmake -P bump-version.cmake
 
 if(NOT DKML_RELEASE_PARTICIPANT_MODULE)
-    message(FATAL_ERROR "Missing -D DKML_RELEASE_PARTICIPANT_MODULE=.../DkMLReleaseParticipant.cmake")
+    message(FATAL_ERROR "Missing -D DKML_RELEASE_PARTICIPANT_MODULE=.../DkMLBumpVersionParticipant.cmake")
 endif()
 include(${DKML_RELEASE_PARTICIPANT_MODULE})
 
-DkMLReleaseParticipant_PlainReplace(README.md)
-DkMLReleaseParticipant_OpamReplace(dkml-runtime-distribution.opam)
-DkMLReleaseParticipant_PkgsReplace(src/none/ci-${DKML_RELEASE_OCAML_VERSION}-pkgs.txt)
-DkMLReleaseParticipant_CreateOpamSwitchReplace(src/unix/create-opam-switch.sh)
-DkMLReleaseParticipant_DuneProjectReplace(dune-project)
-DkMLReleaseParticipant_GitAddAndCommit()
+DkMLBumpVersionParticipant_PlainReplace(README.md)
+DkMLBumpVersionParticipant_OpamReplace(dkml-runtime-distribution.opam)
+DkMLBumpVersionParticipant_PkgsReplace(src/none/ci-${DKML_RELEASE_OCAML_VERSION}-pkgs.txt)
+DkMLBumpVersionParticipant_CreateOpamSwitchReplace(src/unix/create-opam-switch.sh)
+DkMLBumpVersionParticipant_DuneProjectReplace(dune-project)
+DkMLBumpVersionParticipant_GitAddAndCommit()
